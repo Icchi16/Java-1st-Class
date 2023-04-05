a3221ab876add13f5344ab9f1c00c5b9cda783b5
Icchi16 <duynghiemhn16@gmail.com>
2023-04-05 21:27:28 +0700

    better solution

--- src/String1.java
+++ src/String1.java
@@ -1,43 +1,23 @@
-import java.util.Arrays;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class String1 {
     public static void main(String[] args) {
         String string = "aababcacdeeda2314nnmtgh";
-        char[] result = compareChar(string);
-        for (char c: result
-             ) {
-            System.out.print(c + ", ");
-        }
-    }
-
-    public static char[] compareChar(String string){
-        char[] result = new char[0];
-        for (int i = 0; i < string.length(); i++){
-            if (i == 0){
-              result = addChar(result,string.charAt(i));
-            }
-            if (i > 0) {
+        for (int i = 0; i < string.length(); i++) {
+            if (i == 0) {
+                System.out.print(string.charAt(i) + ", ");
+            } else {
                 int loopCount = 0;
-                for (char c : result) {
-                    if (c == string.charAt(i)) {
+                for (int j = 0; j < i; j++) {
+                    if (string.charAt(j)==string.charAt(i)) {
                         loopCount++;
                         break;
                     }
                 }
-                if (loopCount == 0){
-                    result = addChar(result, string.charAt(i));
+                if (loopCount == 0) {
+                    System.out.print(string.charAt(i) + ", ");
                 }
             }
         }
-
-        return  result;
+      }
     }
-
-    public static char[] addChar(char[] resultArr, char newChar){
-        char[] result = Arrays.copyOf(resultArr, resultArr.length + 1);
-        result[result.length - 1] = newChar;
-        return result;
-    }
-}
