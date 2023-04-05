c0100a6b68c492eba32b1e7601034e87431b299a
Icchi16 <duynghiemhn16@gmail.com>
2023-04-05 12:51:13 +0700

    add element to array function done

--- src/String1.java
+++ src/String1.java
@@ -1,4 +1,5 @@
 import java.sql.Array;
+import java.util.Arrays;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,14 +7,28 @@ public class String1 {
     public static void main(String[] args) {
         String string = "aababcacdeeda";
         char[] result = new char[0];
-        for (int i = 0; i < string.length(); i++) {
-            if (i == 0) {
-               result = new char[] addChar(result, i);
-            }
-        }
+        char[] arrtest = {'a','d','f'};
+//        int[] newarr = Arrays.copyOf(arrtest,5);
+//        for (int i: arrtest
+//             ) {
+//            System.out.println(i);
+//        }
+//        for (int i: newarr
+//             ) {
+//            System.out.println(i);
+//        }
+
+
+//        for (int i = 0; i < string.length(); i++) {
+//            if (i == 0) {
+//               result = new char[] addChar(result, i);
+//            }
+//        }
     }
 
-    public static void addChar(char[] resultArr, int charIndex){
-
+    public static char[] addChar(char[] resultArr, char newChar){
+        char[] result = Arrays.copyOf(resultArr, resultArr.length + 1);
+        result[result.length - 1] = newChar;
+        return result;
     }
 }
