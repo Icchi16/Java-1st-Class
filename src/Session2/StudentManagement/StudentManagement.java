645cf211061b5e7fa5621bceb352e4629203f35d
Icchi16 <duynghiemhn16@gmail.com>
2023-04-17 20:12:14 +0700

    tất cả chỉ tại ku =3

--- src/Session2/StudentManagement/StudentManagement.java
+++ src/Session2/StudentManagement/StudentManagement.java
@@ -29,6 +29,7 @@ public class StudentManagement {
                     showListStudents();
                     break;
                 case 2:
+                    themStudent();
                     break;
                 case 3:
                     break;
@@ -50,39 +51,98 @@ public class StudentManagement {
 
     public void showListStudents () {
         System.out.println("---------------- Students ----------------");
+        int studentCount = 0;
         for ( Student student: studentsList
              ) {
-            System.out.println(student);
+            studentCount++;
+            System.out.println("Học sinh " + studentCount + ": [Tên: " + student.getName() + " | Giới tính: " + (student.isGender() ? "Nam" : "Nữ") + " | Độ tuổi: " + student.getAge() + " | Địa chỉ: " + student.getAddress() + " | Tình trạng: " + (student.isStatus() ? "Còn sống" : "Toang rồi ông giáo ạ =<"));
         }
         System.out.println("---------------- Students ----------------");
     }
 
-    public boolean themStudent (Student student) {// nhập id tự tăng
+    public boolean themStudent () {// nhập id tự tăng
+        System.out.println("---------------- Add Student ----------------");
+
+        System.out.println("Vui lòng nhập tên học sinh");
+        String newStudentName = scanner.nextLine();
+
+        System.out.println("Vui lòng chọn giới tính học sinh mới");
+        System.out.println("Nam  -----> 1");
+        System.out.println("Nữ -------> 2");
+        Boolean newStudentGender = null;
+        while (newStudentGender == null) {
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1:
+                    newStudentGender = true;
+                    break;
+                case 2:
+                    newStudentGender = false;
+                    break;
+                default:
+                    System.out.println("Đọc yêu cầu cho kỹ vào!!! Nhá");
+                    break;
+            }
+        }
+
+        System.out.println("Vui lòng nhập độ tuổi học sinh mới");
+        int newStudentAge = Integer.parseInt(scanner.nextLine());
+        System.out.println("Vui lòng nhập địa chỉ học sinh mới");
+        String newStudentAddress = scanner.nextLine();
+        System.out.println("Học sinh còn sống hay đã ...");
+        System.out.println("Sống ------> 1");
+        System.out.println("... -------> 2");
+        Boolean newStudentStatus = null;
+        while (newStudentStatus == null) {
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1:
+                    newStudentStatus = true;
+                    break;
+                case 2:
+                    newStudentStatus = false;
+                    break;
+                default:
+                    System.out.println("Đọc yêu cầu cho kỹ vào!!! Nhá");
+                    break;
+            }
+        }
+        int newStudentId = getNewId(studentsList);
+
+        Student newStudent = new Student(newStudentId,newStudentName,newStudentGender,newStudentAge,newStudentAddress,newStudentStatus);
+
         Student newStudentsList[] = new Student[studentsList.length + 1];
+
         for (int i = 0; i < studentsList.length; i++) {
             newStudentsList[i] = studentsList[i];
         }
-        newStudentsList[newStudentsList.length - 1] = student;
+
+        newStudentsList[newStudentsList.length - 1] = newStudent;
+
         studentsList = newStudentsList;
+        System.out.println("---------------- Đã thêm học sinh thành công ----------------");
         return true;
+
+
+
+
     }
 
     public void botStudent () {
-        System.out.println("Nhập Id Student muốn thủ tiêu");
+        System.out.println("---------------- Remove Student ----------------");
+        System.out.println("Vui lòng nhập Id học sinh muốn thủ tiêu");
         int id = Integer.parseInt(scanner.nextLine());
         Student newStudentsList[] = new Student[studentsList.length - 1];
         if ( findById(id) != null) {
-            int currentOldStudentListIndex = 0;
+            int indexModifier = 0;
             for (int i = 0; i < newStudentsList.length; i++) {
                 if (studentsList[i].getId() != id) {
-                    newStudentsList[i] = studentsList[i + currentOldStudentListIndex];
+                    newStudentsList[i] = studentsList[i + indexModifier];
                 } else {
-                    currentOldStudentListIndex++;
-                    newStudentsList[i] = studentsList[i + currentOldStudentListIndex];
+                    indexModifier++;
+                    newStudentsList[i] = studentsList[i + indexModifier];
                 }
             }
             studentsList = newStudentsList;
-            System.out.println("Thủ tiêu thành công!");
+            System.out.println("---------------- Thủ tiêu thành công ----------------");
         }
     }
 
@@ -94,5 +154,17 @@ public class StudentManagement {
             }
         }
         return null;
+    }
+
+    public int getNewId(Student[] students) {
+        int maxId = 0;
+        for (Student student: students
+             ) {
+            if (student.getId() > maxId) {
+                maxId = student.getId();
+            }
+        }
+        maxId++;
+        return maxId;
     }
 }
